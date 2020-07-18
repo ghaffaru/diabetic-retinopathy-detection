5b801751e5e1d4c5dc33a6be91687453726adda1
Ghaffaru <mudashiruagm@gmail.com>
2020-07-18 17:07:11 +0000

    load data from train,test,val dirs

---
+++ load_data.py
@@ -0,0 +1,10 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+TRAIN_DIR = os.path.join(BASE_DIR, 'train')
+
+VALIDATION_DIR = os.path.join(BASE_DIR, 'validation')
+
+TEST_DIR = os.path.join(BASE_DIR, 'test')
+
